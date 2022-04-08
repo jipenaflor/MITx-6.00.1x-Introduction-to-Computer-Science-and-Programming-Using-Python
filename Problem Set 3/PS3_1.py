a4d2cd1989994368ae9225c38e5aaf98ae28fb29
jipenaflor <92682456+jipenaflor@users.noreply.github.com>
2022-04-08 13:19:35 +0800

    Update PS3_1.py

--- Problem Set 3/PS3_1.py
+++ Problem Set 3/PS3_1.py
@@ -1,5 +1,5 @@
-#Implement the function isWordGuessed that takes in two parameters - a string, secretWord, and a list of letters, lettersGuessed. 
-#This function returns a boolean - True if secretWord has been guessed (ie, all the letters of secretWord are in lettersGuessed) and False otherwise.
+"""Implement the function isWordGuessed that takes in two parameters - a string, secretWord, and a list of letters, lettersGuessed. 
+This function returns a boolean - True if secretWord has been guessed (ie, all the letters of secretWord are in lettersGuessed) and False otherwise."""
 
 def isWordGuessed(secretWord, lettersGuessed):
     '''
