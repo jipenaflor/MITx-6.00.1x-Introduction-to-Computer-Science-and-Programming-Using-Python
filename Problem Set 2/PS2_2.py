6450a9f35963b96be0d71773b27c0a3d08b91da6
jipenaflor <92682456+jipenaflor@users.noreply.github.com>
2022-04-08 13:01:55 +0800

    Update PS2_2.py

--- Problem Set 2/PS2_2.py
+++ Problem Set 2/PS2_2.py
@@ -1,9 +1,15 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Jun 24 14:04:17 2021
+#Now write a program that calculates the minimum fixed monthly payment needed in order pay off a credit card balance within 12 months. 
+#By a fixed monthly payment, we mean a single number which does not change each month, but instead is a constant amount that will be paid each month.
+#In this problem, we will not be dealing with a minimum monthly payment rate.
 
-@author: Jerome Penaflor
-"""
+#The following variables contain values as described below:
+    #balance - the outstanding balance on the credit card
+    #annualInterestRate - annual interest rate as a decimal
+#The program should print out one line: the lowest monthly payment that will pay off all debt in under 1 year
+
+#Assume that the interest is compounded monthly according to the balance at the end of the month (after the payment for that month is made).
+#The monthly payment must be a multiple of $10 and is the same for all months.
+#Notice that it is possible for the balance to become negative using this payment scheme, which is okay.
 
 balance = 3926 #3329, 310 #4773, 440
 annualInterestRate = 0.2
